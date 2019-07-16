f11db885b31e33418b946a20a971afc89d46370a
sardor <sarkamedo@gmail.com>
2019-07-15 23:21:28 -0400

    fixed import issue

--- tests/home_tests/test_hotel_search.py
+++ tests/home_tests/test_hotel_search.py
@@ -1,3 +1,8 @@
+# if you experience issues with importing local modules try commenting following two lines...
+import sys
+sys.path.append(".")
+
+from pages.home_pages.main_page import MainPage
 
 import unittest
 from selenium import webdriver
@@ -7,24 +12,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait as WW
 from selenium.webdriver.support import expected_conditions as EC
 
-from pages.home_pages.main_page import MainPage
 
 
 class ExpediaSearchTests(unittest.TestCase):
-
-    # def test_can_search_and_list_results(self):
-
-    #     baseUrl = "https://www.expedia.com"
-    #     driver = webdriver.Chrome()
-    #     driver.maximize_window()
-    #     driver.get(baseUrl)
-
-    #     mp = MainPage(driver)
-    #     mp.search_hotels_and_show_results("key west")
-    #     list_of_results = WW(driver, 10).until(
-    #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".listing__link.uitk-card-link")))
-    #     self.assertEqual(len(list_of_results), 20)
-    #     driver.quit()
 
     def test_can_search_and_display_results(self):
 
